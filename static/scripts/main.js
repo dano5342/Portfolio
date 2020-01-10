28c87952dc84a61af452bd56532f0e218595f8be
dano5342 <ellis.daniel0@gmail.com>
2020-01-10 14:53:00 +0100

    canvas added onto html, JavaScript removed for side nav, added matrix scrolling effect with help of a youtube tutorial

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,16 +1,37 @@
-$(document).ready(() => {
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+$(document).ready(function(){
+  var canv = document.getElementById("canv");
+  var ctrl = canv.getContext("2d");
 
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+  canv.height = window.innerHeight;
+  canv.width = window.innerWidth;
 
-  // Or with jQuery
+  var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+  matrix = matrix.split("");
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-})
+  var font_size = 12;
+  var columns = canv.width/font_size;
+
+  var drops = [];
+  for(var i = 0; i < columns ; i++)
+    drops[i] = 1;
+    function draw(){
+      ctrl.fillStyle = "rgba(0, 0, 0, 0.04)";
+      ctrl.fillRect(0, 0, canv.width, canv.height);
+
+      ctrl.fillStyle = "#E8175D";//green text
+            ctrl.font = font_size + "Titillium Web";
+            for(var i = 0; i < drops.length; i++)
+            {
+
+                var text = matrix[Math.floor(Math.random()*matrix.length)];
+                ctrl.fillText(text, i*font_size, drops[i]*font_size);
+
+                if(drops[i]*font_size > canv.height && Math.random() > 0.975)
+                    drops[i] = 0;
+
+                drops[i]++;
+            }
+        }
+
+        setInterval(draw, 35);
+});
