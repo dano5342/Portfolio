7de5d78f74386fbf4f7faee66b5cb60174c4b251
dano5342 <ellis.daniel0@gmail.com>
2020-01-14 16:31:04 +0100

    EasyPieChart imported into lib, used to create skill charts for different languages. Will work on spacing these and see what can be done about more information on knowledge. Chart Style rules added in conformity with color scheme

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   var canv = document.getElementById("canv");
   var ctrl = canv.getContext("2d");
@@ -10,43 +10,65 @@ $(document).ready(function(){
   matrix = matrix.split("");
 
   var font_size = 12;
-  var columns = canv.width/font_size;
+  var columns = canv.width / font_size;
 
   var drops = [];
-  for(var i = 0; i < columns ; i++)
+  for (var i = 0; i < columns; i++)
     drops[i] = 1;
-    function draw(){
-      ctrl.fillStyle = "rgba(0, 0, 0, 0.04)";
-      ctrl.fillRect(0, 0, canv.width, canv.height);
 
-      ctrl.fillStyle = "#E8175D";//green text
-            ctrl.font = font_size + "Titillium Web";
-            for(var i = 0; i < drops.length; i++)
-            {
+  function draw() {
+    ctrl.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctrl.fillRect(0, 0, canv.width, canv.height);
 
-                var text = matrix[Math.floor(Math.random()*matrix.length)];
-                ctrl.fillText(text, i*font_size, drops[i]*font_size);
+    ctrl.fillStyle = "#E8175D"; //green text
+    ctrl.font = font_size + "Titillium Web";
+    for (var i = 0; i < drops.length; i++) {
 
-                if(drops[i]*font_size > canv.height && Math.random() > 0.975)
-                    drops[i] = 0;
+      var text = matrix[Math.floor(Math.random() * matrix.length)];
+      ctrl.fillText(text, i * font_size, drops[i] * font_size);
 
-                drops[i]++;
-            }
-        }
+      if (drops[i] * font_size > canv.height && Math.random() > 0.975)
+        drops[i] = 0;
 
-        setInterval(draw, 35);
+      drops[i]++;
+    }
+  }
+
+  setInterval(draw, 35);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
-  
+
 });
 
-window.onscroll = function() {scroller()};
+window.onscroll = function () {
+  scroller()
+};
 
-function scroller(){
-  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200){
+function scroller() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
     document.getElementById("cards").className = "slideLeft";
   }
 }
+
+$(function () {
+  $('.chart').easyPieChart({
+    scaleColor: "#A8A7A7",
+    lineWidth: 15,
+    lineCap: 'butt',
+    barColor: '#E8175D',
+    trackColor: "#474747",
+    size: 180,
+    animate: ({
+      duration: 5500,
+      enabled: true
+    })
+  });
+  $('.chart').data('easyPieChart').disableAnimation();
+
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    $('.chart').data('easyPieChart').enableAnimation();
+  };
+});
